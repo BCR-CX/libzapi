bb2284f1a55feba33c8518bcc1cfe6081a2043c6
Leandro Meili <lmeilibr@users.noreply.github.com>
2025-12-21 00:15:56 -0300

    Lm/custom object field (#19)
    * add custom object field model
    
    * add custom object fields

--- libzapi/infrastructure/api_clients/custom_data/__init__.py
+++ libzapi/infrastructure/api_clients/custom_data/__init__.py
@@ -1,5 +1,7 @@
 from libzapi.infrastructure.api_clients.custom_data.custom_object import CustomObjectApiClient
+from libzapi.infrastructure.api_clients.custom_data.custom_object_field import CustomObjectFieldApiClient
 
 __all__ = [
     "CustomObjectApiClient",
+    "CustomObjectFieldApiClient",
 ]
