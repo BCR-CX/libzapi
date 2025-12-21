8c01c0a1455c74c6ea4ffa238e5f7f42939e320e
Leandro Meili <lmeilibr@users.noreply.github.com>
2025-12-20 23:11:20 -0300

    add custom objects (#18)

---
+++ libzapi/application/services/custom_data/__init__.py
@@ -0,0 +1,21 @@
+from libzapi.application.services.custom_data.custom_objects_service import CustomObjectsService
+from libzapi.infrastructure.http.auth import oauth_headers, api_token_headers
+from libzapi.infrastructure.http.client import HttpClient
+import libzapi.infrastructure.api_clients.custom_data as api
+
+
+class CustomData:
+    def __init__(
+        self, base_url: str, oauth_token: str | None = None, email: str | None = None, api_token: str | None = None
+    ):
+        if oauth_token:
+            headers = oauth_headers(oauth_token)
+        elif email and api_token:
+            headers = api_token_headers(email, api_token)
+        else:
+            raise ValueError("Provide oauth_token or email+api_token")
+
+        http = HttpClient(base_url, headers=headers)
+
+        # Initialize services
+        self.custom_objects = CustomObjectsService(api.CustomObjectApiClient(http))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,32 @@
 import os
+from typing import Type, TypeVar
 
 import pytest
 
-from libzapi import Ticketing, HelpCenter
+from libzapi import Ticketing, HelpCenter, CustomData
+
+T = TypeVar("T")
 
 
 @pytest.fixture(scope="session")
 def ticketing():
     """Creates a real Ticketing client if environment variables are set."""
-    base_url = os.getenv("ZENDESK_URL")
-    email = os.getenv("ZENDESK_EMAIL")
-    api_token = os.getenv("ZENDESK_TOKEN")
+    return _generic_zendesk_client(Ticketing)
 
-    if not (base_url and email and api_token):
-        pytest.skip("Zendesk credentials not provided. Skipping live API tests.")
 
-    return Ticketing(
-        base_url=base_url,
-        email=email,
-        api_token=api_token,
-    )
+@pytest.fixture(scope="session")
+def custom_data():
+    """Creates a real Help Center client if environment variables are set."""
+    return _generic_zendesk_client(CustomData)
 
 
 @pytest.fixture(scope="session")
 def help_center():
     """Creates a real Help Center client if environment variables are set."""
+    return _generic_zendesk_client(HelpCenter)
+
+
+def _generic_zendesk_client(client_cls: Type[T]) -> T:
     base_url = os.getenv("ZENDESK_URL")
     email = os.getenv("ZENDESK_EMAIL")
     api_token = os.getenv("ZENDESK_TOKEN")
@@ -32,7 +34,7 @@ def help_center():
     if not (base_url and email and api_token):
         pytest.skip("Zendesk credentials not provided. Skipping live API tests.")
 
-    return HelpCenter(
+    return client_cls(
         base_url=base_url,
         email=email,
         api_token=api_token,
